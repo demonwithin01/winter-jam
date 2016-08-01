e3599eee1d3b371d15ac0034a59cded66378a4f6
Stephen Barnes <barnesy@outlook.com>
2016-08-01 22:01:09 +1000

    Update

---
+++ SewerGame/Assets/Scripts/DialogTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DialogTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private List<string> _text;
+    
+    private bool _hasTriggered = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter( Collider collider )
+    {
+        if ( _hasTriggered == false && collider.gameObject.tag == "Player" )
+        {
+            _hasTriggered = true;
+
+            foreach ( var text in _text )
+            {
+                Globals.Dialog.AddText( text );
+            }
+        }
+    }
+}
